8ccd7e55187bc4cc323847c2aa46ee1dc5ca3533
cedricdcc <cedricdecruw@gmail.com>
2026-09-09 10:01:51 +0200

    chore(website): scaffold isolated vite react demonstration app

---
+++ website/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  resolve: {
+    alias: {
+      wrx: path.resolve(__dirname, '../src/index.ts'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+});
